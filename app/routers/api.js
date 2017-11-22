d94784279e66f46b5843321af2921aaff36034dc
ChinLi <kananek.ati@hotmail.com>
2017-11-22 20:15:13 +0000

    finish add question

--- app/routers/api.js
+++ app/routers/api.js
@@ -9,6 +9,7 @@ router.post('/createQuestion',function(req,res){
     newProblem.question = req.body.question;
     newProblem.correctAnswer = req.body.correctAnswer;
     newProblem.choices = req.body.choices;
+    newProblem.quizId = req.body.quizId;
     newProblem.save(function(err,problem){
         if(err){
             res.json(err);
@@ -57,6 +58,42 @@ router.post('/createQuiz',function(req,res){
             res.json(err);
         }else{
             res.json({"message":"Already created quiz"});
+        }
+    })
+})
+router.post("/removeQuestion",function(req,res){
+    var id = req.body.id;
+    Problem.findOne({'_id':id},function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            result.quizId = null
+            result.save(function(err){
+                if(err){
+                    res.json({"error":true});
+                }else{
+                    res.json({"error":false});
+                }
+            })
+        }
+    })
+})
+
+router.post("/addQuestion",function(req,res){
+    var quizId = req.body.quizId;
+
+    Problem.findOne({'_id':req.body.id},function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            result.quizId = quizId;
+            result.save(function(err){
+                if(err){
+                    res.json({"error":true});
+                }else{
+                    res.json({"error": false});
+                }
+            })
         }
     })
 })
--- app/routers/pages.js
+++ app/routers/pages.js
@@ -4,12 +4,24 @@ var path = require('path');
 var Problems = require('../models/problem')
 var Quiz = require('../models/quiz');
 
-router.get("/",function(req,res){
+router.get('/',function(req,res){
     Quiz.find({},function(err,result){
         if(err){
             console.log(err);
         }else{
-            res.render('home',{data: result});
+            res.render('home',{data:result})
+        }
+    })
+})
+router.get('/staff',function(req,res){
+    res.render('staff');
+})
+router.get("/quizzes",function(req,res){
+    Quiz.find({},function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('quizzes',{data: result});
         }
     })
 })
@@ -45,9 +57,39 @@ router.get('/quiz/edit/:id',function(req,res){
         if(err){
             res.json({"error":true,"message":err});
         }else{
-            res.render('editQuiz',{data: result});
+            //problem in quiz
+            Problems.find({'quizId': result._id},function(err,result1){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(result1);
+                    res.render('editQuiz',{data: result, problems : result1 });
+                }
+            })
         }
     })
 });
+
+router.get('/quiz/addQuestion/:id',function(req,res){
+    var id = req.params.id;
+    Problems.find({'quizId': {$ne: id}},function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("addQuestion",{data: result,id : id});
+        }
+    })
+})
+
+/*
+find problem which is not contain the quiz id 
+ Problems.find({'_id': {$ne: result._id}},function(err,result2){
+                        if(err){
+                            console.log(err);
+                        }else{
+                        }
+ })
+
+*/
 
 module.exports = router;
