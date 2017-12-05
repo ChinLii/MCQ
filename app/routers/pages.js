adfc81c6bd204856fd2e439b8f02111c0806c79d
ChinLi <kananek.ati@hotmail.com>
2017-12-05 03:39:35 +0000

    fix topic system

--- app/routers/pages.js
+++ app/routers/pages.js
@@ -50,7 +50,6 @@ router.get('/createQuestion',function(req,res){
         if(err){
             console.log(err);
         }else{
-            console.log(result);
             res.render('createQuestion',{data: result});
         }
     })
